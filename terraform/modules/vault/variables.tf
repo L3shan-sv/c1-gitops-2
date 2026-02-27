1d3dd5d76cd960a0c79da202a3623696f6e34f44
mac <mac@l3shans-MacBook-Pro.local>
2026-02-27 13:38:09 +0300

    added varibles necessary for the vault

---
+++ terraform/modules/vault/variables.tf
@@ -0,0 +1,4 @@
+variable "environment"       { type = string }
+variable "vault_irsa_role_arn" { type = string }
+variable "cluster_endpoint"  { type = string }
+variable "cluster_ca_cert"   { type = string }
